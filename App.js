d082189550031079a88e4ef7b709a275610493a3
Nizam <ramazanov570633@gmail.com>
2023-12-19 00:28:16 +0400

    Day_Weather_Flatlist

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
 import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          'http://api.weatherapi.com/v1/forecast.json?key=b53d68e3e4894fba901174157231712&q=istanbul&days=7&aqi=no'
+          'http://api.weatherapi.com/v1/forecast.json?key=b53d68e3e4894fba901174157231712&q=London&days=7&aqi=no&alerts=no'
         );
         setData(response.data);
+
       } catch (error) {
         setError(error);
       }
@@ -21,33 +21,88 @@ const App = () => {
     fetchData();
   }, []);
 
-  // const renderitem = (data)=>{
-  //   return(
+  if (error) {
+    return (
+      <View>
+        <Text>Error occurred: {error.message}</Text>
+      </View>
+    );
+  }
 
-  //   )
-  // }
+  if (!data) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  const renderitem = ({item}) => {
+    const dateString = item.forecast.forecastday.date;
+    const dateObject = new Date(dateString);
+    const dayOfWeek = dateObject.toLocaleDateString('en-US', { weekday: 'long' });
+    return (
+      <View>
+        <Text style={{ fontSize: 30 }}>{dayOfWeek}</Text>
+        <Image
+          source={{ uri: `https:${item.forecast.forecastday.day.condition.icon}` }}
+          style={{ width: 64, height: 64 }}
+        />
+        <Text>{item.forecast.forecastday.day.maxtemp_c}</Text>
+      </View>
+    )
+  }
+
 
   return (
     <View style={stayle.container}>
       <View>
-        {/* <Text style={stayle.text}>{data && data.location.name}</Text>
+        <Text style={stayle.text}>{data.location.name}</Text>
         <View style={stayle.view}>
           <Text style={stayle.text1}>{data.location.country}</Text>
           <Text style={stayle.text2}>{data.location.localtime}</Text>
-        </View> */}
+        </View>
       </View>
-      <View>
+      <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
         <View>
-          <View style={stayle.tempview}>
-            <Text style={stayle.temp}>{data.current.temp_c}</Text>
-            <Text style={stayle.tempd}>O</Text>
+          <View style={{ marginTop: '20%' }}>
+            <View style={stayle.tempview}>
+              <Text style={stayle.temp}>{data.current.temp_c}</Text>
+              <Text style={stayle.tempd}>O</Text>
+            </View>
+            <View style={stayle.tempview1}>
+              <Image
+                source={{ uri: `https:${data.current.condition.icon}` }}
+                style={{ width: 64, height: 64, marginRight: '5%' }}
+              />
+              <Text style={{ color: '#FFFFFF', fontSize: 20, fontWeight: '300', marginTop: '10%' }}>{data.current.condition.text}</Text>
+            </View>
           </View>
-          <View style={stayle.tempview1}>
-            <Text style={{ color: '#FFFFFF', fontSize: 20 }}>{data.current.condition.text}</Text>
+        </View>
+        <View style={{ width: 2, backgroundColor: 'gray', height: '100%', marginTop: '10%' }}></View>
+        <View style={{ marginTop: '20%' }}>
+          <View>
+            <Text style={{ color: '#FFFFFF', fontWeight: '300', fontSize: 20 }}>Wind</Text>
+            <Text style={{ color: '#FFFFFF', marginTop: '10%', fontSize: 15 }}>{data.current.wind_mph} km</Text>
+          </View>
+          <View style={{ marginTop: '20%' }}>
+            <Text style={{ color: '#FFFFFF', fontWeight: '300', fontSize: 20 }}>Humidity</Text>
+            <Text style={{ color: '#FFFFFF', marginTop: '10%', fontSize: 15 }}>%{data.current.humidity}</Text>
+          </View>
+          <View style={{ marginTop: '20%' }}>
+            <Text style={{ color: '#FFFFFF', fontWeight: '300', fontSize: 20 }}>Cloud</Text>
+            <Text style={{ color: '#FFFFFF', marginTop: '10%', fontSize: 15 }}>%{data.current.cloud}</Text>
           </View>
         </View>
       </View>
-
+      <View>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => renderitem(item)}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
+      <Text>ewghjasgdbwjk</Text>
     </View>
   );
 };
@@ -80,7 +135,6 @@ const stayle = StyleSheet.create({
   },
   temp: {
     fontSize: 100,
-    marginLeft: '15%',
     color: '#FFFFFF',
     fontWeight: '200',
     marginTop: '30%'
@@ -96,8 +150,10 @@ const stayle = StyleSheet.create({
     flexDirection: 'row'
   },
   tempview1: {
-    marginLeft: '15%',
-    fontWeight: '200',
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: '10%'
+
   }
 })
 export default App;
